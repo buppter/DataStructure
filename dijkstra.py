77b31ded44403513d53de6d9dd46df919e84411d
sxtian <shixintian@aliyun.com>
2019-04-01 10:04:33 +0800

    dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -4,19 +4,19 @@ import math
 
 graph = {
 
-    "A": ["B": 5, "C": 1],
-    "B": ["A": 5, "C": 2, "D": 1],
-    "C": ["A": 1, "B": 2, "D": 4, "E": 8],
-    "D": ["B": 1, "C": 4, "E": 3, "F": 6],
-    "E": ["C": 8, "D": 3],
-    "F": ["D": 6]
+    "A": {"B": 5, "C": 1},
+    "B": {"A": 5, "C": 2, "D": 1},
+    "C": {"A": 1, "B": 2, "D": 4, "E": 8},
+    "D": {"B": 1, "C": 4, "E": 3, "F": 6},
+    "E": {"C": 8, "D": 3},
+    "F": {"D": 6}
 }
 
 
-def init_distance(graph, s):
-    distance = {s, 0}
+def init_distance(graph, start):
+    distance = {start : 0}
     for vertex in graph:
-        if vertex != s:
+        if vertex != start:
             distance[vertex] = math.inf
     return distance
 
@@ -26,10 +26,10 @@ def dijkstra(graph, start):
     heapq.heappush(pqueue, (0, start))
     seen = set()
     parent = {start: None}
-    distance = init_distance(graph, s)
+    distance = init_distance(graph, start)
 
     while len(pqueue) > 0:
-        pair = heapq.heappop(0)
+        pair = heapq.heappop(pqueue)
         dist = pair[0]
         vertex = pair[1]
         seen.add(start)
